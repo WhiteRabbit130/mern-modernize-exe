5dc57bbd8b684391675139eea2859bea5b1ee905
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-07 19:02:14 +0200

    make left-header

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,20 @@
 import logo from '../assets/logo.svg';
 import './app.css';
+import { makeStyles } from '@mui/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { IconMenu2, IconSearch, IconShoppingCart, IconBellRinging } from '@tabler/icons-react';
+import { IconMenu2, IconSearch, IconShoppingCart, IconBellRinging, IconChevronDown } from '@tabler/icons-react';
 import flagEnSvg from '../assets/flags/icon-flag-en.svg';
+
+const buttonStyles = {
+  borderRadius: '7px',
+  textTransform: "none",
+  fontWeight: 400,
+  padding: "5px 15px 5px 15px"
+}
 
 function App() {
   return (
@@ -18,19 +30,9 @@ function App() {
       </div>
 
       <div className="container">
-        <header className='header'>
-          <div className="left-header">
-            <IconButton aria-label="menu">
-              <IconMenu2 
-                  size={20}
-                  color="currentColor"
-                  stroke={2}
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                />
-                <span className="hover-grey-background"></span>
-            </IconButton>
-            {/* <button className="btn-control-navbar" tabindex="0" type="button" aria-label="menu">
+        <AppBar color='inherit' position="sticky" sx={{boxShadow: 'none'}}>
+          <Toolbar>
+            <IconButton aria-label="menu" color="inherit">
               <IconMenu2 
                 size={20}
                 color="currentColor"
@@ -38,36 +40,43 @@ function App() {
                 strokeLinejoin="round"
                 strokeLinecap="round"
               />
-              <span className="hover-grey-background"></span></button> */}
-            <button className="btn-search-menu grey-background" tabindex="0" type="button" aria-label="show 4 new mails" aria-controls="search-menu" aria-haspopup="true">
-               <IconSearch
+            </IconButton>
+            <IconButton aria-label="search-menu" color="inherit" size="large">
+              <IconSearch
                 size={16}
                 color="currentColor"
                 stroke={2}
                 strokeLinejoin="round"
                 strokeLinecap="round"
               />
-               <span className="hover-grey-background"></span></button>
-            <button className="header-apps-dropmenu" tabindex="0" type="button" aria-label="show 11 new notifications" aria-controls="msgs-menu" aria-haspopup="true">
-              Apps
-              <span className="dropdown-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-down" width="15" height="15" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <polyline points="6 9 12 15 18 9"></polyline>
-                </svg>
-              </span>
-              <span className=""></span>
-            </button>
-            <a href="/" className="header-menu">Chat</a>
-            <a href="/" className="header-menu">Calendar</a>
-            <a href="/" className="header-menu">Email</a>
-          </div>
-          <div className="right-header">
-            <div className="lang">
+            </IconButton>
+            <Box>
+              <span>
+                <Button color='inherit' size="medium" sx={buttonStyles}>
+                  Apps
+                  <span>
+                  <IconButton aria-label="menu" color="inherit">
+                    <IconChevronDown 
+                      size={15}
+                      color="currentColor"
+                      stroke={2}
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
+                    />
+                  </IconButton>
+                  </span>
+                </Button>
+              </span>             
+            </Box>
+            <Button color='inherit' size="medium" sx={buttonStyles}>Chat</Button>
+            <Button color='inherit' size="medium" sx={buttonStyles}>Calendar</Button>
+            <Button color='inherit' size="medium" sx={buttonStyles}>Email</Button>           
+            <Box sx={{ flexGrow: 1 }}></Box>
+            {/* <div className="lang">
               <img src={flagEnSvg} alt="en" className='lang-menu-en' />
-            </div>
+            </div> */}
             {/* <div className="shopping-cart rh-ml"> */}
-            <span className="icon-shopping-cart rh-ml">
+            {/* <span className="icon-shopping-cart rh-ml">
               <IconShoppingCart
                 size={21}
                 color="currentColor"
@@ -76,10 +85,10 @@ function App() {
                 strokeLinecap="round"
               />
               <span className="show-shopping-cart topRight">0</span>
-            </span>
+            </span> */}
             {/* </div> */}
             {/* <div className="notification rh-ml"> */}
-              <span className="icon-notification rh-ml">
+              {/* <span className="icon-notification rh-ml">
                 <IconBellRinging
                   size={21}
                   color="currentColor"
@@ -88,17 +97,18 @@ function App() {
                   strokeLinecap="round"
                 />
                 <span className="show-notification topRight"></span>
-              </span>
+              </span> */}
             {/* </div> */}
             {/* <div className="profile rh-ml"> */}
-              <button type='button' className="btn-profile rh-ml">
+              {/* <button type='button' className="btn-profile rh-ml">
                 <div className="profile">
                   <img src="/assets/img/user-1.jpg" alt="user-1" className='icon-profile'/>
                 </div>
-              </button>
+              </button> */}
             {/* </div> */}
-          </div>          
-        </header>
+          </Toolbar>
+        </AppBar>
+      
 
         <div className="main-content">
           <div className="contents">
